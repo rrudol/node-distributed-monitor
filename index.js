217d31bb9e6a06497c6e2c373bdab71149f2ead5
Rafał Rudol <rrrrudol@gmail.com>
2018-05-10 08:59:23 +0200

    add automation for peers configuration

--- index.js
+++ index.js
@@ -1,46 +1,42 @@
+const fetch = require('node-fetch');
 const ProducerConsumerMonitor = require('./src/ProducerConsumerMonitor');
 
-const monitor = new ProducerConsumerMonitor({
-  peers: [
-    // 'tcp://150.254.45.138:9312',
-    // 'tcp://150.254.45.138:9313',
-    // 'tcp://150.254.45.138:9314',
-    // 'tcp://150.254.45.138:9315',
-    // 'tcp://150.254.45.138:9316',
-    // 'tcp://150.254.45.138:9317',
-    'tcp://192.168.1.2:6001',
-    'tcp://192.168.1.3:6002',
-    // 'tcp://192.168.1.3:9312',
-    // 'tcp://192.168.1.3:9313',
-    // 'tcp://192.168.1.3:9314',
-    // 'tcp://192.168.1.3:9315',
-    // 'tcp://192.168.1.3:9316',
-    // 'tcp://192.168.1.3:9317'
-  ],
-  port: process.argv[3]
-});
+fetch('http://sirius.cs.put.poznan.pl/~inf122510/peers.json')
+    .then(res => res.json())
+    .then(json => run(json));
 
-monitor.name = process.argv[3] % 2 ? 'producer' : 'consumer';
+function run(peers) {
 
-setTimeout(() => {
+  console.log(peers)
 
-  async function start() {
-    let i;
-    for (i = 0; i < 10; i++) {
-      await new Promise((resolve, reject) => {
-        setTimeout(() => {
-          if (process.argv[3] % 2) {
-            monitor.produce('a').then(() => resolve());
-          } else {
-            monitor.consume('a').then(() => resolve());
-          }
-        }, Math.random() * 500 + 600)
-      });
+  const monitor = new ProducerConsumerMonitor({
+    peers,
+    port: process.argv[3]
+  });
+  
+  monitor.name = process.argv[3] % 2 ? 'producer' : 'consumer';
+  
+  setTimeout(() => {
+  
+    async function start() {
+      let i;
+      for (i = 0; i < 10; i++) {
+        await new Promise((resolve, reject) => {
+          setTimeout(() => {
+            if (process.argv[3] % 2) {
+              monitor.produce('a').then(() => resolve());
+            } else {
+              monitor.consume('a').then(() => resolve());
+            }
+          }, Math.random() * 500 + 600)
+        });
+      }
+      setTimeout(() => {
+        console.log('done', monitor.buffer)
+      }, 2000);
     }
-    setTimeout(() => {
-      console.log('done', monitor.buffer)
-    }, 2000);
-  }
-  start();
+    start();
+  
+  }, 1000);
 
-}, 1000);
+}
